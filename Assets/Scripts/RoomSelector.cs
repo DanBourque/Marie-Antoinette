f864a251ab270c19ebd37a1ceb2ca33394a6eece
Dan Bourque <Dan.Bourque@gmail.com>
2021-06-21 18:19:22 -0300

    Scale up the currently selected room. Slow down the rotation.

--- Assets/Scripts/RoomSelector.cs
+++ Assets/Scripts/RoomSelector.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public enum Room{ ChainLocker, EngineRoom, Galley, GuestCabin, Quarters, Sailroom, Salon }
 
 public class RoomSelector: MonoBehaviour{
-	private const float SmoothTime = .01f, deckExpandedY = 1.73f, hullExpandedY = -2.91f;
+	private const float SmoothTime = .01f, deckExpandedY = 1.73f, hullExpandedY = -2.91f, expandedScale = 1.5f;
 	public Transform ship, hull, deck, spotlight;
 	public GameObject[] rooms;
 	private Vector3[] roomAnchors;
 	private Vector3[] roomPosTargets;
+	private Vector3[] roomScaleTargets;
 	private Vector3 hullPosTarget, deckPosTarget;
 
 	private void Awake(){
 		roomAnchors = new Vector3[ rooms.Length ];
 		roomPosTargets = new Vector3[ rooms.Length ];
+		roomScaleTargets = new Vector3[ rooms.Length ];
 		for( var r=0; r<roomAnchors.Length; r++ ){		// Record the original anchors for animation purposes.
 			roomAnchors[ r ] = rooms[ r ].transform.Find( "in" ).localPosition;
 			roomPosTargets[ r ] = Vector3.zero;
+			roomScaleTargets[ r ] = Vector3.one;
 		}
 	}
 
@@ -30,21 +33,29 @@ public class RoomSelector: MonoBehaviour{
 	private void SelectRoom( Room room, bool isOn ){
 //		rooms[ ( int )room ].SetActive( isOn );
 		if( isOn ){
-			for( var r=0; r<rooms.Length; r++ )
+			for( var r=0; r<rooms.Length; r++ ){
 				roomPosTargets[ r ] = new Vector3( 0, hullExpandedY, 0 );
+				roomScaleTargets[ r ] = Vector3.one;
+			}
 			roomPosTargets[ ( int )room ] = spotlight.position-roomAnchors[ ( int )room ];
+			roomScaleTargets[ ( int )room ] = new Vector3( expandedScale, expandedScale, expandedScale );
 			deckPosTarget = new Vector3( 0, deckExpandedY, 0 );
 			hullPosTarget = new Vector3( 0, hullExpandedY, 0 );
 		}else{
-			for( var r=0; r<rooms.Length; r++ )
+			for( var r=0; r<rooms.Length; r++ ){
 				roomPosTargets[ r ] = Vector3.zero;
+				roomScaleTargets[ r ] = Vector3.one;
+			}
 			deckPosTarget = hullPosTarget = Vector3.zero;
 		}
 	}
 
 	private void Update(){
-		for( var r=0; r<rooms.Length; r++ )
+		for( var r=0; r<rooms.Length; r++ ){
 			rooms[ r ].transform.localPosition = Vector3.Lerp( rooms[ r ].transform.localPosition, roomPosTargets[ r ], SmoothTime );
+			rooms[ r ].transform.localScale	= Vector3.Lerp( rooms[ r ].transform.localScale, roomScaleTargets[ r ], SmoothTime );
+		}
+
 		deck.transform.localPosition = Vector3.Lerp( deck.transform.localPosition, deckPosTarget, SmoothTime );
 		hull.transform.localPosition = Vector3.Lerp( hull.transform.localPosition, hullPosTarget, SmoothTime );
 	}
--- Assets/Scripts/RotationControls.cs
+++ Assets/Scripts/RotationControls.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
 public class RotationControls: MonoBehaviour{
+	private float Speed = .1f;
 	private Vector3 prevMousePos = Vector3.zero, mousePosDelta = Vector3.zero;
 
 	private void Update(){
 		if( Input.GetMouseButton( 0 ) ){
-			mousePosDelta = Input.mousePosition - prevMousePos;
+			mousePosDelta = ( Input.mousePosition-prevMousePos )*Speed;
 			if( Vector3.Dot( transform.up, Vector3.up ) > 0 )
 				transform.Rotate( transform.up, -Vector3.Dot( mousePosDelta, Camera.main.transform.right ), Space.World );
 			else
