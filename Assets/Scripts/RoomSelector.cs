6233873828ff9c94431b7d10def5419873991097
Dan Bourque <Dan.Bourque@gmail.com>
2021-06-22 22:58:05 -0300

    Created a uniform hull mesh, and the stern's cap, and applied textures UV mapped on the model. Placed all the sections' buttons on a slideout panel to the right.

--- Assets/Scripts/RoomSelector.cs
+++ Assets/Scripts/RoomSelector.cs
@@ -4,22 +4,24 @@ public enum Room{ ChainLocker, EngineRoom, Galley, GuestCabin, Quarters, Sailroo
 
 public class RoomSelector: MonoBehaviour{
 	private const float SmoothTime = 3, deckExpandedY = 1.73f, hullExpandedY = -2.91f, expandedScale = 1.5f;
-	public Transform ship, hull, deck, spotlight;
+	public Transform hull, deck, spotlight;
 	public GameObject[] rooms;
 	private Vector3[] roomAnchors;
 	private Vector3[] roomPosTargets;
 	private Vector3[] roomScaleTargets;
 	private Vector3 hullPosTarget, deckPosTarget;
+	public RectTransform slideoutToggle, slideoutPanel;
+	private bool isSlideoutVisible = true;
 
 	private void Awake(){
 		roomAnchors = new Vector3[ rooms.Length ];
 		roomPosTargets = new Vector3[ rooms.Length ];
 		roomScaleTargets = new Vector3[ rooms.Length ];
 		for( var r=0; r<roomAnchors.Length; r++ ){		// Record the original anchors for animation purposes.
-			roomAnchors[ r ] = rooms[ r ].transform.Find( "in" ).localPosition;
-			roomPosTargets[ r ] = Vector3.zero;
+			roomAnchors[ r ] = roomPosTargets[ r ] = rooms[ r ].transform.localPosition;
 			roomScaleTargets[ r ] = Vector3.one;
 		}
+		OnSectionsSlideout();
 	}
 
 	public void OnChainLocker( bool isOn ) => SelectRoom( Room.ChainLocker, isOn );
@@ -31,23 +33,28 @@ public class RoomSelector: MonoBehaviour{
 	public void OnSalon( bool isOn ) => SelectRoom( Room.Salon, isOn );
 
 	private void SelectRoom( Room room, bool isOn ){
-//		rooms[ ( int )room ].SetActive( isOn );
 		if( isOn ){
 			for( var r=0; r<rooms.Length; r++ ){
-				roomPosTargets[ r ] = new Vector3( 0, hullExpandedY, 0 );
+				roomPosTargets[ r ] = roomAnchors[ r ] + new Vector3( 0, hullExpandedY, 0 );
 				roomScaleTargets[ r ] = Vector3.one;
 			}
-			roomPosTargets[ ( int )room ] = spotlight.position-roomAnchors[ ( int )room ];
+			roomPosTargets[ ( int )room ] = spotlight.position;
 			roomScaleTargets[ ( int )room ] = new Vector3( expandedScale, expandedScale, expandedScale );
 			deckPosTarget = new Vector3( 0, deckExpandedY, 0 );
 			hullPosTarget = new Vector3( 0, hullExpandedY, 0 );
 		}else{
 			for( var r=0; r<rooms.Length; r++ ){
-				roomPosTargets[ r ] = Vector3.zero;
+				roomPosTargets[ r ] = roomAnchors[ r ];
 				roomScaleTargets[ r ] = Vector3.one;
 			}
 			deckPosTarget = hullPosTarget = Vector3.zero;
 		}
+	}
+
+	public void OnSectionsSlideout(){
+		isSlideoutVisible = !isSlideoutVisible;
+		slideoutPanel.anchoredPosition = new Vector2( isSlideoutVisible ? 0 : slideoutPanel.rect.width, 0 );
+		slideoutToggle.anchoredPosition = new Vector2( isSlideoutVisible ? -slideoutPanel.rect.width : 0, 0 );
 	}
 
 	private void Update(){
@@ -55,10 +62,7 @@ public class RoomSelector: MonoBehaviour{
 
 		for( var r=0; r<rooms.Length; r++ ){
 			rooms[ r ].transform.localPosition = Vector3.Lerp( rooms[ r ].transform.localPosition, roomPosTargets[ r ], time );
-			var @in = rooms[ r ].transform.Find( "in" );
-			var @out = rooms[ r ].transform.Find( "out" );
-			@in.localScale	= Vector3.Lerp( @in.localScale, roomScaleTargets[ r ], time );
-			@out.localScale	= Vector3.Lerp( @out.localScale, roomScaleTargets[ r ], time );
+			rooms[ r ].transform.localScale	= Vector3.Lerp( rooms[ r ].transform.localScale, roomScaleTargets[ r ], time );
 		}
 
 		deck.transform.localPosition = Vector3.Lerp( deck.transform.localPosition, deckPosTarget, time );
