3a166b14d4a0f79bc940995c9e712a852c1f2a26
Dan Bourque <Dan.Bourque@gmail.com>
2021-07-15 15:21:27 -0300

    Added script to open/close the engine room hatch & extend/retract the slideout table. Added hardwood floor texture to the galley. Built & deployed

---
+++ Assets/Scripts/HatchOpener.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class HatchOpener: MonoBehaviour{
+	private const float TableTravel = 0.6f;
+	public float speed = 0.25f;
+	public Transform table;
+	private Vector3 initialTablePos;
+	private float angle = 0;
+
+	private void Awake() => initialTablePos = table.localPosition;
+
+	private void Update(){
+		angle += speed;
+		if( angle>360 )
+			angle -= 360;
+
+		var sin = Mathf.Sin( Mathf.Deg2Rad*angle );
+		transform.localEulerAngles = new Vector3( Mathf.Min( sin*-90, 0 ), 0, 0 );
+		table.transform.localPosition = initialTablePos + new Vector3( 0, Mathf.Min( sin*TableTravel, 0 ), 0 );
+	}
+}
