99e4fe8a0dc40e31761afcadc18ea8543b375b53
Dan Bourque <Dan.Bourque@gmail.com>
2021-07-30 11:47:52 -0300

    Added an animation controller on the Camera Dolly, to allow us to create "tours" around the ship. Added a Tour toggle button to the top controls. Removed the camera zoom lerping.

--- Assets/Scripts/RotationControls.cs
+++ Assets/Scripts/RotationControls.cs
@@ -1,23 +1,46 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class RotationControls: MonoBehaviour{
-	private const float ZoomSpeed = .5f, ZoomMin = -14, ZoomMax = 7, LerpDuration = .5f;
+	private const float ZoomSpeed = .5f, ZoomMin = -14, ZoomMax = 7;
 	public float dragSpeed = .25f, orbitSpeed = 0.025f;
-	private bool isRotating = true;
-	public Transform spotlight, camDolly;
+	private bool isRotating = true, isTouring = false, isDragging = false;
+	public Transform spotlight;
 	private Camera cam;
-	private Vector3 prevMousePos = Vector3.zero, mousePosDelta = Vector3.zero;
-	private bool isDragging = false;
-	private float timeElapsed = LerpDuration, startCamZ, targetCamZ;
+	private Vector3 prevMousePos = Vector3.zero, mousePosDelta = Vector3.zero, preTourCamPosition;
+	private Quaternion preTourCamRotation;
+	private Transform camDolly, uiControls;
+//	private Toggle rotateToggle, tourToggle;
 
 	private void Awake(){
 		cam = Camera.main;
 		camDolly = cam.transform.parent;
-		startCamZ = targetCamZ = Mathf.Clamp( cam.transform.localPosition.z, ZoomMin, ZoomMax );
+		uiControls = FindObjectOfType< Canvas >( true ).transform;
+//		rotateToggle = uiControls.Find( "Top Controls/Rotate Toggle" ).GetComponent< Toggle >();
+//		tourToggle = uiControls.Find( "Top Controls/Tour Toggle" ).GetComponent< Toggle >();
 	}
 
 	public void SetRotating( bool value ) => isRotating = value;
+
+	public void SetTouring( bool value ){
+		isTouring = value;
+		if( isTouring ){
+			preTourCamPosition = camDolly.transform.localPosition;
+			preTourCamRotation = camDolly.transform.localRotation;
+		}else{
+			camDolly.transform.localPosition = preTourCamPosition;
+			camDolly.transform.localRotation = preTourCamRotation;
+		}
+		ShowUIControls( !isTouring );
+		camDolly.GetComponent< Animator >().enabled = isTouring;
+	}
+
+	private void ShowUIControls( bool show ){
+		uiControls.Find( "Room Selector" ).gameObject.SetActive( show );
+		uiControls.Find( "Photos" ).gameObject.SetActive( show );
+		uiControls.Find( "X-Ray Controls" ).gameObject.SetActive( show );
+	}
 
 	private void Update(){
 		var eventSystem = EventSystem.current;
@@ -39,18 +62,7 @@ public class RotationControls: MonoBehaviour{
 			camDolly.transform.RotateAround( spotlight.position, Vector3.up, orbitSpeed );
 
 		var scrollwheelDelta = Input.mouseScrollDelta;
-		if( scrollwheelDelta.y!=0 && !eventSystem.IsPointerOverGameObject() ){
-			startCamZ = cam.transform.localPosition.z;
-			targetCamZ = Mathf.Clamp( targetCamZ+scrollwheelDelta.y*ZoomSpeed, ZoomMin, ZoomMax );
-			timeElapsed = 0;
-		}
-
-		var time = timeElapsed/LerpDuration;
-		time = time * time * ( 3f-2f*time );
-		if( timeElapsed<LerpDuration ){
-			cam.transform.localPosition = new Vector3( 0, 0, Mathf.Lerp( startCamZ, targetCamZ, time ) );
-			timeElapsed += Time.deltaTime;
-		}else if( cam.transform.localPosition.z!=targetCamZ )
-			cam.transform.localPosition = new Vector3( 0, 0, targetCamZ );
+		if( scrollwheelDelta.y!=0 && !eventSystem.IsPointerOverGameObject() )
+			cam.transform.localPosition = new Vector3( 0, 0, Mathf.Clamp( cam.transform.localPosition.z+scrollwheelDelta.y*ZoomSpeed, ZoomMin, ZoomMax ) );
 	}
 }
